fa258501f0462e4d76cc3955d35193368c355317
pratyushsinha213 <154828922+pratyushsinha213@users.noreply.github.com>
2025-11-28 23:02:54 +0400

    Time: 0 ms (100%), Space: 18 MB (35.73%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
@@ -4,7 +4,7 @@ class Solution:
         diff = arr[1]-arr[0]
 
         for i in range(1, len(arr)-1):
-            if arr[i+1] - arr[i] != diff:
+            if diff != arr[i+1] - arr[i]:
                 return False
         
         return True
