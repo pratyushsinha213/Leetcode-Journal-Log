bb5069b83d466f0ab6eaa8ad04483a430d59bd70
pratyushsinha213 <154828922+pratyushsinha213@users.noreply.github.com>
2025-11-28 22:42:17 +0400

    Time: 7 ms (61.48%), Space: 17.9 MB (8.67%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxPower(self, s: str) -> int:
+        max_power = 1
+        current_power = 1
+
+        for i in range(1, len(s)):
+            if s[i] == s[i - 1]:
+                current_power += 1
+            else:
+                max_power = max(max_power, current_power)
+                current_power = 1
+        
+        # Final check to update max_power for the last contiguous substring
+        max_power = max(max_power, current_power)
+        
+        return max_power
+            
