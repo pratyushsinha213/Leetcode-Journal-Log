83c1888942e8cce9c46b74ad99db269607c543a2
pratyushsinha213 <154828922+pratyushsinha213@users.noreply.github.com>
2025-11-29 14:26:25 +0400

    Time: 0 ms (100%), Space: 17.9 MB (20.26%) - LeetHub

---
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.py
@@ -0,0 +1,19 @@
+class Solution:
+    def squareIsWhite(self, coordinates: str) -> bool:
+        char_map = {
+            "a": 1,
+            "b": 2,
+            "c": 3,
+            "d": 4,
+            "e": 5,
+            "f": 6,
+            "g": 7,
+            "h": 8,
+        }
+
+        final_pos = (char_map[coordinates[0]], int(coordinates[1]))
+
+        if (final_pos[0] % 2 == 1 and final_pos[1] % 2 == 1) or (final_pos[0] % 2 == 0 and final_pos[1] % 2 == 0):
+            return False
+        else:
+            return True
