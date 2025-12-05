5fb7c72fbb2038228da26fcb4a6c0700da194350
pratyushsinha213 <154828922+pratyushsinha213@users.noreply.github.com>
2025-12-05 22:45:56 +0400

    Time: 0 ms (100%), Space: 17.9 MB (32.66%) - LeetHub

--- 3360-stone-removal-game/3360-stone-removal-game.py
+++ 3360-stone-removal-game/3360-stone-removal-game.py
@@ -3,12 +3,24 @@ class Solution:
         if n < 10:
             return False
         
-        count = 0
-        pile = 10
+        # count = 0
+        # pile = 10
 
-        while n >= pile:
-            n -= pile
-            pile -= 1
-            count += 1
+        # while n >= pile:
+        #     n -= pile
+        #     pile -= 1
+        #     count += 1
 
-        return count % 2 != 0
+        # return count % 2 != 0
+
+        first = 10
+        isAlice = True
+        while n > 0:
+            if first > n:
+                return not isAlice
+
+            n -= first
+            first -= 1
+            isAlice = not isAlice
+
+        return not isAlice
