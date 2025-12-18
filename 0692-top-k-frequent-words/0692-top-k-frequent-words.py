c34a5ab0f24d796d623cbf86535b26676aa04537
pratyushsinha213 <154828922+pratyushsinha213@users.noreply.github.com>
2025-12-18 18:20:39 +0400

    Time: 0 ms (100%), Space: 17.4 MB (99.98%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,13 +1,26 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        # counter = Counter(words)
+        # word_frequency = [(-freq, word) for word, freq in counter.items()]
+
+        # heapq.heapify(word_frequency)
+
+        # arr = []
+        # for i in range(k):
+        #     most_freq, word = heapq.heappop(word_frequency)
+        #     arr.append(word)
+        
+        # return arr
+
+        heap = []
         counter = Counter(words)
-        word_frequency = [(-freq, word) for word, freq in counter.items()]
-
-        heapq.heapify(word_frequency)
-
+        
+        for word, freq in counter.items():
+            heapq.heappush(heap, (-freq, word))
+        
         arr = []
         for i in range(k):
-            most_freq, word = heapq.heappop(word_frequency)
+            most_freq, word = heapq.heappop(heap)
             arr.append(word)
         
         return arr
