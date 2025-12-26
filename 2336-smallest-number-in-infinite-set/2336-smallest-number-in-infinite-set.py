b1ec29266c965d746b46127c9f33e8ca2538c1b8
pratyushsinha213 <154828922+pratyushsinha213@users.noreply.github.com>
2025-12-26 12:21:24 +0400

    Time: 23 ms (46.1%), Space: 18.4 MB (70.44%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.py
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.py
@@ -1,16 +1,44 @@
+# class SmallestInfiniteSet:
+
+#     def __init__(self):
+#         self.minHeap = [i for i in range(1, 1001)]
+#         heapify(self.minHeap)
+
+#     def popSmallest(self) -> int:
+#         return heappop(self.minHeap) if len(self.minHeap) != 0 else -1
+
+#     def addBack(self, num: int) -> None:
+#         if num in set(self.minHeap):
+#             return
+#         heappush(self.minHeap, num)
+
+
+# # Your SmallestInfiniteSet object will be instantiated and called as such:
+# # obj = SmallestInfiniteSet()
+# # param_1 = obj.popSmallest()
+# # obj.addBack(num)
+
+import heapq
 class SmallestInfiniteSet:
-
+    # Set of all positive integers - initialize this
     def __init__(self):
-        self.list = [i for i in range(1, 1001)]
-        heapify(self.list)
+        # Set to keep removed objects
+        self.removed = set()
+        # Min heap of size k to track min objects
+        self.minheap = list(range(1, 1001))
+        heapq.heapify(self.minheap)
 
+    # Remove and return smallest int
     def popSmallest(self) -> int:
-        return heappop(self.list) if len(self.list) != 0 else -1
+        x = heapq.heappop(self.minheap)
+        self.removed.add(x)
+        return x
 
+    # Add back a positive int num if not already in infinite set
     def addBack(self, num: int) -> None:
-        if num in set(self.list):
-            return
-        heappush(self.list, num)
+        if num in self.removed:
+            heapq.heappush(self.minheap, num)
+            self.removed.remove(num)
 
 
 # Your SmallestInfiniteSet object will be instantiated and called as such:
