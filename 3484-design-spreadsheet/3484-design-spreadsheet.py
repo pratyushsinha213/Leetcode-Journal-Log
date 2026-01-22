87638254becca5cd91622555d404f07785eb3c05
Pratyush Sinha <154828922+pratyushsinha213@users.noreply.github.com>
2026-01-22 12:58:19 +0400

    Time: 135 ms (44.14%), Space: 26 MB (11.72%) - LeetHub

--- 3484-design-spreadsheet/3484-design-spreadsheet.py
+++ 3484-design-spreadsheet/3484-design-spreadsheet.py
@@ -3,19 +3,21 @@ class Spreadsheet:
     def __init__(self, rows: int):
         self.matrix = [[0] * 26 for _ in range(rows)]
 
-    def _get_cell_value(self, cell: str) -> int:
+    def _get_rows_cols(self, cell):
         col = ord(cell[0]) - ord('A')
         row = int(cell[1:]) - 1
+        return [col, row]
+
+    def _get_cell_value(self, cell: str) -> int:
+        col, row = self._get_rows_cols(cell)
         return self.matrix[row][col]
 
     def setCell(self, cell: str, value: int) -> None:
-        col = ord(cell[0]) - ord('A')
-        row = int(cell[1:]) - 1
+        col, row = self._get_rows_cols(cell)
         self.matrix[row][col] = value
 
     def resetCell(self, cell: str) -> None:
-        col = ord(cell[0]) - ord('A')
-        row = int(cell[1:]) - 1
+        col, row = self._get_rows_cols(cell)
         self.matrix[row][col] = 0
 
     def getValue(self, formula: str) -> int:
